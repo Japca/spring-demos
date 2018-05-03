4c94bfcd9e8e3205322919a1fe14b1b38f4d981a
jakub krhovjak <jakub.krhojvak@aevi.cz>
2018-05-03 21:08:55 +0200

    Phaser basic2

--- testframeworksdemo/src/main/java/com/example/demo/PhaserTest.java
+++ testframeworksdemo/src/main/java/com/example/demo/PhaserTest.java
@@ -16,21 +16,29 @@ public class PhaserTest {
 
 	public static void main(String[] args) throws InterruptedException {
 		ExecutorService executorService = Executors.newCachedThreadPool();
-		Phaser phaser = new Phaser(1);
+		Phaser phaser = new Phaser();
 		log.info("Main Parties {}" , phaser.getRegisteredParties());
 		log.info("Main unarived {}" , phaser.getUnarrivedParties());
 		log.info("Phase: {} ", phaser.getPhase());
 
-		executorService.execute(new Task("thread-1", phaser));
-		executorService.execute(new Task("thread-2", phaser));
-		executorService.execute(new Task("thread-3", phaser));
-		log.info("Phase: {} ", phaser.getPhase());
-		TimeUnit.SECONDS.sleep(2);
+		Task task1 = new Task("thread-1", phaser);
+		Task task2 = new Task("thread-2", phaser);
+		Task task3 = new Task("thread-3", phaser);
+
 		log.info("Main Parties {}" , phaser.getRegisteredParties());
 		log.info("Main unarived {}" , phaser.getUnarrivedParties());
-		phaser.arriveAndDeregister();
 		log.info("Phase: {} ", phaser.getPhase());
-		log.info("End fmain thread");
+		executorService.execute(task1);
+		executorService.execute(task2);
+		TimeUnit.SECONDS.sleep(5);
+		executorService.execute(task3);
+//		log.info("Phase: {} ", phaser.getPhase());
+//		TimeUnit.SECONDS.sleep(2);
+//		log.info("Main Parties {}" , phaser.getRegisteredParties());
+//		log.info("Main unarived {}" , phaser.getUnarrivedParties());
+//		phaser.arriveAndDeregister();
+//		log.info("Phase: {} ", phaser.getPhase());
+//		log.info("End fmain thread");
 
 
 	}
@@ -53,37 +61,29 @@ class Task implements Runnable {
 
 	@Override
 	public void run() {
+		log.info("-------------------------------------------------------------------");
 		log.info("in run");
+		log.info("Parties {}" , phaser.getRegisteredParties());
+		log.info("unarived {}" , phaser.getUnarrivedParties());
+		log.info("Phase: {} ", phaser.getPhase());
 		phaser.arriveAndAwaitAdvance();
-		log.info("after wait");
-		try {
+		log.info("after wait Phase: {} ", phaser.getPhase());
+			try {
 
 			TimeUnit.SECONDS.sleep(3 + i);
-			synchronized (this) {
-				if (i <= 0) {
-					log.info("Parties {}" , phaser.getRegisteredParties());
-					log.info("unarived {}" , phaser.getUnarrivedParties());
-					phaser.register();
-					log.info("Parties {}" , phaser.getRegisteredParties());
-					log.info("unarived {}" , phaser.getUnarrivedParties());
-					++i;
-				}
-			}
+
 		} catch (InterruptedException e) {
 			e.printStackTrace();
 		}
 
+		log.info("-------------------------------------------------------------------");
+		log.info("after wait 2 Phase: {} ", phaser.getPhase());
 		log.info("Parties {}" , phaser.getRegisteredParties());
 		log.info("unarived {}" , phaser.getUnarrivedParties());
-		if(i >= 1) {
-			phaser.arriveAndDeregister();
-			log.info("Parties {}" , phaser.getRegisteredParties());
-			log.info("unarived {}" , phaser.getUnarrivedParties());
-		}
+
+
 		phaser.arriveAndAwaitAdvance();
-		log.info("Phase: {} ", phaser.getPhase());
-		log.info("Parties {}" , phaser.getRegisteredParties());
-		log.info("unarived {}" , phaser.getUnarrivedParties());
+
 		log.info("End thread: {} ", Thread.currentThread().getName());
 	}
 }
